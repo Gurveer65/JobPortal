ce7bcead28dd54797a6979f448f7c530850a9459
Gurveer65 <gurveerkaur813@gmail.com>
2025-07-24 19:20:04 +0530

    commit6

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.generics import ListAPIView
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view
