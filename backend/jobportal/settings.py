adcca9bc3d2fc06b09e5d462f9acc9f37f1c0474
Gurveer65 <gurveerkaur813@gmail.com>
2025-07-24 14:47:56 +0530

    commit

--- backend/jobportal/settings.py
+++ backend/jobportal/settings.py
@@ -8,9 +8,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=(way)e2#c-1w!zf@8h$!1=b(v_i9v5@7)16r0bi54yr#c8bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://backend-0ddt.onrender.com', 
+]
 
 # Application definition
 INSTALLED_APPS = [
