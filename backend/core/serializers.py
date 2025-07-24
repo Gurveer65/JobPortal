6af0bb026da80678861493b1376a56ce85aa94df
Gurveer65 <gurveerkaur813@gmail.com>
2025-07-24 20:21:44 +0530

    commit2

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -51,8 +51,9 @@ class RegisterSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     email = serializers.EmailField(source='user.email', read_only=True)
+    profile_icon = serializers.URLField(allow_blank=True, required=False)
 
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'username', 'email', 'bio']
+        fields = ['id', 'user', 'username', 'email', 'bio', 'profile_icon']
         read_only_fields = ['user', 'username', 'email']
