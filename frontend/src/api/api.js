81df366260021f9b92d6525617f14a5a7696e0fc
Gurveer65 <gurveerkaur813@gmail.com>
2025-07-24 09:59:48 +0530

    commit4

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  // baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://backend-v1qw.onrender.com/api/'
 });
 
 API.interceptors.request.use((config) => {
